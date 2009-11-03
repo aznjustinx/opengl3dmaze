c2ccd435dae763e3594a5189e99ad160624a917a
stefane81 <stefane81@46c2c806-c88e-11de-9233-4169c94807ec>
2009-11-03 21:52:53 +0000

    maze út frá array

--- Maze.cpp
+++ Maze.cpp
@@ -14,6 +14,22 @@ Tölvugrafik
 
 using namespace std;
 
+GLuint texture; //the array for our texture
+GLuint texture2; //the array for our second texture
+
+int cMap[10][10] = { //our map
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 1, 0, 1, 0, 0, 0, 1},
+	{1, 0, 0, 1, 0, 1, 0, 0, 0, 1},
+	{1, 0, 0, 1, 1, 1, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+};
+
 Maze::Maze()
 {
 
@@ -25,10 +41,52 @@ Maze::~Maze()
 
 
 // draws 3D box
+void Maze::drawMaze()
+{
+	
+	glTranslatef(0, 0, 0); //translate back a bit to view the map correctly
+	drawBox();
+		
+	glLoadIdentity(); // Til að núlla hreyfingu á angle
+}
+
+
+
+
+// draws 3D box
 void Maze::drawBox()
 {
+	for (int i = 0; i < 10; i++) //loop through the height of the map
+	{
+		for (int j = 0; j < 10; j++) //loop through the width of the map
+		{
+			if (cMap[i][j] == 0) //if the map at this position contains a 0
+			{				
+				//glBindTexture( GL_TEXTURE_2D, texture ); //bind our grass texture to our shape
+			}
+			else //otherwise
+			{
+				glColor3f(0,1,0);
+				glPushMatrix(); //push the matrix so that our translations only affect this tile
+			
+				glTranslatef(j*3, 0, -i*3); //translate the tile to where it should belong
+
+				drawWall();
+	
+				glEnd();
+			glPopMatrix(); //pop the matrix
+				//glBindTexture( GL_TEXTURE_2D, texture2 ); //bind our dirt texture to our shape
+			}
+			
+			
+		} //end first loop
+	} //end second loop
+}
+
+void Maze::drawWall()
+{
 	//glTranslatef(0.0f, 0.0f, -8.0f); // zooma út
-	glColor3f(1.0f, 1.0f, 0.0f);
+	//glColor3f(1.0f, 1.0f, 0.0f);
 	glBegin(GL_QUADS);
 	
 	//Front
