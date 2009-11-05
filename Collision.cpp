b9979cae392a890f258dcc92176967aff7889b26
stefane81 <stefane81@46c2c806-c88e-11de-9233-4169c94807ec>
2009-11-05 05:31:04 +0000

    Eitthvað smá Collision detection. En buggy. Þurfum að endurskoða if skilyrðin..

--- Collision.cpp
+++ Collision.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Collision.h"
+#include "math.h"
 
 
 using namespace std;
@@ -17,16 +18,23 @@ void Collision::init(Player* pl, Maze* ma)
 bool Collision::check()
 {
 	Point3* pos = player->getPosition();
-	cout<<"Collision eye: x: "<<pos->getX()<<" y: "<<pos->getY()<<" z: "<<pos->getZ()<<"\n";
+	cout<<"Collision eye: x: "<<int(pos->getX())<<" y: "<<int(pos->getY())<<" z: "<<int(pos->getZ())<<"\n";
 	
 	Point3* ma = maze->getPosition();
+	//cout<<"Collision wall: x: "<<ma[3].getX()<<" y: "<<ma[3].getY()<<" z: "<<ma[3].getZ()<<"\n";
 	for (int i = 0; i < maze->getNumberOfWalls(); ++i) {
 		float xx =  ma[i].getX() + 1.5;
+		
+		//cout<<"Collision wall:"<<i<<": x: "<<ma[i].getX()<<" y: "<<ma[i].getY()<<" z: "<<ma[i].getZ()<<"\n";
 
-
-
-		if (ma[i].getX() + 1.5 == player->getPosition()->getX() ||  ma[i].getX() - 1.5 == player->getPosition()->getX() ||
-			ma[i].getZ() + 1.5 == player->getPosition()->getZ() ||  ma[i].getZ() - 1.5 == player->getPosition()->getZ()) {
+		/*if (ma[i].getX() + 10.5 == player->getPosition()->getX() ||  ma[i].getX() - 1.5 == player->getPosition()->getX() ||
+			ma[i].getZ() + 1.5 == player->getPosition()->getZ() ||  ma[i].getZ() - 1.5 == player->getPosition()->getZ()) 
+		{*/
+		if (	fabs(ma[i].getX() + 1.5 -(pos->getX())) < 0.1
+			||  fabs(ma[i].getX() - 1.5 -(pos->getX())) < 0.1
+			||	fabs(ma[i].getZ() + 1.5 -(pos->getZ())) < 0.1
+			||  fabs(ma[i].getZ() - 1.5 -(pos->getZ())) < 0.1 ) 
+		{
 			return true;
 		}
 
