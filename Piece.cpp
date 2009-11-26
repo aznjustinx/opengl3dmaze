aad28444ef04b4a4255b38fc6dd6298ad1257992
stefane81 <stefane81@46c2c806-c88e-11de-9233-4169c94807ec>
2009-11-26 19:17:14 +0000

    Display = false

--- Piece.cpp
+++ Piece.cpp
@@ -16,6 +16,7 @@ Piece::Piece(void)
 	glGenTextures(PIECE_MAX_TEXTURES, g_PieceTextures);
 	main::loadImage(g_PieceTextures[TEX_GOODIE], ".\\pacman.tif");
 	rotAngle = 0;
+	captured = false;
 }
 
 Piece::~Piece(void)
@@ -33,20 +34,23 @@ void Piece::update()
 
 void Piece::display()
 {
-	main::materialColor(1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.);
-	glBindTexture( GL_TEXTURE_2D, g_PieceTextures[TEX_GOODIE] );
-	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-	glPushMatrix();
-	glRotatef(rotAngle, 0., 1., 0.);
-	glTranslatef(-PIECE_SIZE/2, -PIECE_SIZE/2, 0);
-	
-	main::makePlate(PIECE_SIZE, PIECE_SIZE, NR_OF_VERTS, NR_OF_VERTS, PIECE_SIZE, PIECE_SIZE);
-	glPopMatrix();
-	
-	glPushMatrix();
-	glRotatef(rotAngle + 180, 0., 1., 0.);
-	glTranslatef(-PIECE_SIZE/2 - 0.001, -PIECE_SIZE/2, -0.001);
-	
-	main::makePlate(PIECE_SIZE, PIECE_SIZE, NR_OF_VERTS, NR_OF_VERTS, PIECE_SIZE, PIECE_SIZE);
-	glPopMatrix();
+	if(captured==false)
+	{
+		main::materialColor(1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.);
+		glBindTexture( GL_TEXTURE_2D, g_PieceTextures[TEX_GOODIE] );
+		glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+		glPushMatrix();
+		glRotatef(rotAngle, 0., 1., 0.);
+		glTranslatef(-PIECE_SIZE/2, -PIECE_SIZE/2, 0);
+		
+		main::makePlate(PIECE_SIZE, PIECE_SIZE, NR_OF_VERTS, NR_OF_VERTS, PIECE_SIZE, PIECE_SIZE);
+		glPopMatrix();
+		
+		glPushMatrix();
+		glRotatef(rotAngle + 180, 0., 1., 0.);
+		glTranslatef(-PIECE_SIZE/2 - 0.001, -PIECE_SIZE/2, -0.001);
+		
+		main::makePlate(PIECE_SIZE, PIECE_SIZE, NR_OF_VERTS, NR_OF_VERTS, PIECE_SIZE, PIECE_SIZE);
+		glPopMatrix();
+	}
 }
